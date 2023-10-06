b9babbb37d615221be8b60b22e4b5215638f3385
Rithickg <rithickg567@gmail.com>
2023-10-06 09:22:43 +0530

    removed js

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.mobile-menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-toggleButton.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
